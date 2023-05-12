d425134e3c8ef4449427949f20d5f3a58b0d35be
Ibrahim Elhag <ielhag@live.com>
2023-05-12 14:27:12 -0700

    lambda function update

--- amplify/backend/function/lambda8020prcnt/src/index.py
+++ amplify/backend/function/lambda8020prcnt/src/index.py
@@ -1,15 +1,18 @@
 import json
 
+
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+    print(event)
+    customerId = event['pathParameters']['customerId']
+    customer = {'customerId': customerId, 'customerName': "Customer " + customerId}
+    response = {
+        'statusCode': 200,
+        # uncomment below to enable CORS requests
+        'headers': {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*"
+        },
+        'body': json.dumps(customer)
+    }
+
+    return response
