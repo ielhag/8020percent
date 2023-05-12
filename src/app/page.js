6a099a720f3b3767bdbcd6dffa8baf41ec340bdf
Ibrahim Elhag <ielhag@live.com>
2023-05-12 16:34:05 -0700

    ui components edit page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,19 @@
 'use client'
 
-import { ThemeProvider } from "@aws-amplify/ui-react";
-import { Amplify } from 'aws-amplify';
+import {ThemeProvider} from "@aws-amplify/ui-react";
+import {Amplify} from 'aws-amplify';
 
 import awsconfig from './aws-exports';
 
 import "@aws-amplify/ui-react/styles.css";
-import { studioTheme } from "./ui-components";
+import {HeroLayout2, studioTheme} from "./ui-components";
 
 Amplify.configure(awsconfig);
 
 export default function Home() {
-  return (
-      <ThemeProvider theme={studioTheme}>
-
-      </ThemeProvider>
-  )
+    return (
+        <ThemeProvider theme={studioTheme}>
+            <HeroLayout2/>
+        </ThemeProvider>
+    )
 }
