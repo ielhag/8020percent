630ea5d074a9260853c647d804bc5bdc33465237
Ibrahim Elhag <ielhag@live.com>
2023-05-15 05:46:51 -0700

    homepage structure

--- src/app/page.js
+++ src/app/page.js
@@ -6,14 +6,16 @@ import {Amplify} from 'aws-amplify';
 import awsconfig from './aws-exports';
 
 import "@aws-amplify/ui-react/styles.css";
-import {HeroLayout2, studioTheme} from "./ui-components";
+import {HeroLayout2, NavBar, MarketingFooter, studioTheme} from "./ui-components";
 
 Amplify.configure(awsconfig);
 
 export default function Home() {
     return (
         <ThemeProvider theme={studioTheme}>
+            <NavBar/>
             <HeroLayout2/>
+            <MarketingFooter/>
         </ThemeProvider>
     )
 }
